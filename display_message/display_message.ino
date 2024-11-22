86d05947a0a349ae275d6e12fd1ec94eeca500f4
alexrmelnick <melnick@bu.edu>
2024-11-21 20:02:20 -0500

    Got the display working correctly!

--- display_message/display_message.ino
+++ display_message/display_message.ino
@@ -1,61 +1,66 @@
 /*
   This is the function for running the display. 
+  Acknoledgements: This program uses the 'Extensible hd44780' library by Bill Perry (https://github.com/duinoWitchery/hd44780)
+    Documentation: https://github.com/duinoWitchery/hd44780/wiki/ioClass:-hd44780_I2Cexp
 */
 
 #include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
+#include <hd44780.h> // main hd44780 header
+#include <hd44780ioClass/hd44780_I2Cexp.h> // i2c expander i/o class header
+#include <string.h>
 
-LiquidCrystal_I2C LCD(0x27, 20, 4); // Create an LCD struct with I2C addr 0x27 that is 20 chars wide and 4 lines tall
+hd44780_I2Cexp LCD(0x27, 20, 4); // Create an LCD struct with I2C addr 0x27 that is 20 chars wide and 4 lines tall
 // Verified that I2C addr is 0x27
 
 void setup() {
-  Serial.begin(9600); // Set the baud rate to 9600 baud
+  Serial.begin(9600);
+  LCD.begin(20, 4);
+  LCD.backlight();
 
-  // Initialize the LCD
-  LCD.begin(20, 4); // Need to pass the size of display (20x4 chars) in newer versions of this library
-  delay(2); // Need a delay after each LCD command
-
-  // // ------- Quick 3 blinks of backlight -------------
-  // for(int i = 0; i< 3; i++)
-  // {
-  //   LCD.backlight();
-  //   delay(250);
-  //   LCD.noBacklight();
-  //   delay(250);
-  // }
-  LCD.backlight(); // finish with backlight on
-  delay(2); // Need a delay after each LCD command
-
-  LCD.clear();
-  delay(2); // Need a delay after each LCD command
-
-
-  // Print a test message
-  LCD.setCursor(0,0); // Set the cursor the char 0 on line 0
-  delay(2); // Need a delay after each LCD command
-  LCD.print("I am GROOT");
-  delay(2); // Need a delay after each LCD command
+  String msg1 = "Hello World! This is a long message to test the printMessage. I want it to be really long. That is why I am still typing. So far I am on... the 162nd character. Wow, I love modern IDEs. Isn't it crazy that the VIC-20 only had a 22x23 character display? Imagine trying to get real work done on a display as wide as ours!";
+  printMessage(msg1);
 }
 
 void printMessage(String message) {
-  // Set up the display
-  LCD.clear();
-  LCD.setCursor(0, 0);
+  bool printToMonitor = true; // Option to print to the serial monitor for debugging
+  int pauseTime = 5000; // Amount of time to pause between pages (5 seconds seems about right)
+  int messageLength = message.length();
+  int necessaryPages = (messageLength+80-1) / 80;   // Need to break message up into 80 character 'pages' (rounding up)
 
-  Serial.print(message);
-  LCD.print(message);
+  if(printToMonitor) { // Print data to monitor if desired
+      Serial.print("Message is ");
+      Serial.print(messageLength);
+      Serial.print(" chars, ");
+      Serial.print(messageLength/20);
+      Serial.print(" lines, or ");
+      Serial.print(necessaryPages);
+      Serial.print(" pages long.\n");
+  }
 
+  // This library wraps text incorrectly, so we need to do it ourselves
 
-  // // If message fits on 1 line, just print it
-  // if(message_length < 20) {
-  //   LCD.print(message);
-  // }
+  for(int i = 0; i < necessaryPages; i++) {
+    String page = message.substring(i*80,i*80+80); // Get the current page and its length
+    int pageLength = page.length();
+    int necessaryLines = (pageLength+20-1) / 20; // Get the number of necessary lines, rounding up
 
-  // // If message is multiple lines, we need to handle dividing it into multiple 
-  // // lines and adding dashes to the end of each line that doesn't end in a space
+    LCD.clear();
 
+    for(int j = 0; j < necessaryLines; j++) { // Iterate through each line
+      String currentLine = page.substring(j*20, j*20+20); // Get the line
+      LCD.setCursor(0,j); // Set the cursor to the first row
+      LCD.print(currentLine); // Print the data
+
+      if(printToMonitor) { // Print data to monitor if desired
+        Serial.print(currentLine);
+        Serial.print('\n');
+      }
+    }
+
+    delay(pauseTime); // Pause before printing the next page for the user to read it 
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 }
