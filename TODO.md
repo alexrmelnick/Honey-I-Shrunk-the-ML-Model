b1bc3b975e75c1d458c088aca4bb592d1be85c21
alexrmelnick <melnick@bu.edu>
2024-11-22 11:22:36 -0500

    Trying to get code for conversion working

--- TODO.md
+++ TODO.md
@@ -1,7 +1,6 @@
 # TODO List
 
 - Get the model to run on the Teensy board
-- Get the Teensy audio hat to record audio
 - Get everything to work together
 - Design an inclosure?
 
@@ -10,3 +9,4 @@
 - Quantized the model! To run the quanititization, run `python whisper_quanititization.py`. To run the test inferrence, run `python whisper_inference.py`. 
 - Purchased the parts! We have a Teensy 4.1, the Teensy audio board, and the LCD200420x4 I2C Blue display.
 - Got the display successfully displaying text!
+- Got the Teensy audio hat to record audio
--- convert_model_to_liteRT.py
+++ convert_model_to_liteRT.py
@@ -1,13 +1,27 @@
 import ai_edge_torch
-import numpy
 import torch
+import numpy
 import torchvision
 
-# Load the model
-model = torch.load('Models/quantized_whisper_tiny_en.pth')
+resnet18 = torchvision.models.resnet18(torchvision.models.ResNet18_Weights.IMAGENET1K_V1).eval()
 
-# Convert the model
-edge_model = ai_edge_torch.convert()
+sample_input = (torch.randn(1, 3, 224, 224),)
+edge_model = ai_edge_torch.convert(resnet18.eval(), sample_input)
 
-# Save the model
-edge_model.export('Models/quantized_whisper_tiny_en_liteRT.tflite')
+output = edge_model(*sample_inputs)
+
+
+# import ai_edge_torch
+# import torch
+
+# # Load the model
+# model = torch.load('Models/quantized_whisper_tiny_en/quantized_model.pth')
+
+# # Convert the model
+# print("Starting conversion...")
+# edge_model = ai_edge_torch.convert()
+
+# # Save the model
+# print("Saving the converted model...")
+# edge_model.export('Models/quantized_whisper_tiny_en_liteRT.tflite')
+# print("Model saved successfully.")
