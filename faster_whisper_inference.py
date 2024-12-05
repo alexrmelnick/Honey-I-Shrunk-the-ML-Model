1d50544248c1089ffac04b3e7b2bbef796e05709
[Sergio Rodriguez] <[serseth9@gmail.com]>
2024-12-05 16:47:45 -0500

    Remove venv from tracking

--- faster_whisper_inference.py
+++ faster_whisper_inference.py
@@ -1,8 +1,8 @@
 from faster_whisper import WhisperModel
 
 # Load the Faster-Whisper model
-model_path = "tiny"  # Choose 'tiny', 'base', 'small', 'medium', or 'large'
-model = WhisperModel(model_path, device="cpu")  # Use CPU since you're on a Raspberry Pi
+model_path = "tiny.en"  # Choose 'tiny', 'base', 'small', 'medium', or 'large'
+model = WhisperModel(model_path, device="cpu", compute_type ="int8")  # Use CPU since you're on a Raspberry Pi
 
 # Path to the WAV file
 audio_file = "Whisper_Test.wav"
